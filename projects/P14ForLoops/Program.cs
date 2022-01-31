5cfdcb30c1f807520d6f03f92d36eac196517435
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-31 13:37:22 +0100

    Created projects, added and completed P14

---
+++ projects/P14ForLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P14ForLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("From 1 to 1000");
+            for (int i = 0; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("From 200 to 100");
+            for (int i = 200; i >= 100; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("From 1024 to 1");
+            for (int i = 1024; i >= 1; i /= 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+

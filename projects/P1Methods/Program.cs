a10d68c62e073cc884978c4eef18c93a573187d3
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-31 14:25:41 +0100

    Completed P1Methods

---
+++ projects/P1Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P1Methods
+{
+    class Program
+    {
+        static void CountUp()
+        {
+            Console.WriteLine("Counting from 1 to 5");
+            for (int i = 0; i <= 5; i++)
+            { 
+                Console.WriteLine(i);
+            }
+        }
+        static void CountDown()
+        {
+            Console.WriteLine("Counting from 5 to 0");
+            for (int i = 5; i >= 0; i--)
+            { 
+                Console.WriteLine(i);
+            }
+        }
+        static void Main()
+        {
+            CountUp();
+            CountDown();
+            CountUp();
+            CountDown();
+        }
+    }
+}
+

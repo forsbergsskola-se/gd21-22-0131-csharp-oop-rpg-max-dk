9773600d3db77feab494e8f0feb445b15d297467
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-31 14:12:06 +0100

    Completed P16Arrays

--- projects/P16Arrays/Program.cs
+++ projects/P16Arrays/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace P16Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[11];
+            Random random = new Random();
+            Console.WriteLine("I will roll 10.000 numbers between 0 and 10:");
+
+            for (int i = 0; i <= 10000; i++)
+            {
+                int randomNum = random.Next(0, 11);
+                array[randomNum]++;
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"I rolled {i} a total of {array[i]}");
+            }
+           
+        }
+    }
+    
+    
+}
+

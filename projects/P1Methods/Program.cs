2eb4ce014e3f19a9bfe981f312d06571dd2ff932
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-31 14:29:33 +0100

    Modified P1Methods to complete 2.1 Parameters

--- projects/P1Methods/Program.cs
+++ projects/P1Methods/Program.cs
@@ -2,28 +2,30 @@
 {
     class Program
     {
-        static void CountUp()
+        static void CountUp(int to)
         {
-            Console.WriteLine("Counting from 1 to 5");
-            for (int i = 0; i <= 5; i++)
+            Console.WriteLine($"Counting from 0 to {to}");
+            for (int i = 0; i <= to; i++)
             { 
                 Console.WriteLine(i);
             }
         }
-        static void CountDown()
+        static void CountDown(int from)
         {
-            Console.WriteLine("Counting from 5 to 0");
-            for (int i = 5; i >= 0; i--)
+            Console.WriteLine($"Counting from {from} to 0");
+            for (int i = from; i >= 0; i--)
             { 
                 Console.WriteLine(i);
             }
         }
         static void Main()
         {
-            CountUp();
-            CountDown();
-            CountUp();
-            CountDown();
+            CountUp(1);
+            CountDown(1);
+            CountUp(2);
+            CountDown(2);
+            CountUp(3);
+            CountDown(3);
         }
     }
 }

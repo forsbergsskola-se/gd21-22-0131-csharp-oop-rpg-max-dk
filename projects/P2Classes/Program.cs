a1551367ea3cd664bb907f167025cf6f8df62c63
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-02-15 14:42:38 +0100

    Created and completed P2Classes

---
+++ projects/P2Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿
+    class Program
+    {
+        static void Main()
+        {
+            Person human = new Person();
+            Animal dog = new Animal();
+            Car volvo = new Car();
+
+            Console.WriteLine(human);
+            Console.WriteLine(dog);
+            Console.WriteLine(volvo);
+        }
+    }
+
+    class Person
+    {
+        
+    }
+
+    class Animal
+    {
+        
+    }
+
+    class Car
+    {
+        
+    }
+

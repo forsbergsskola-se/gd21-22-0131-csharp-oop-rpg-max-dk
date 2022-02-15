4d392b8f85cdc0175fe61649bbdd2ceeacb2828a
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-02-15 19:10:54 +0100

    Created and completed assignments 7 to 9 in RPG project

---
+++ projects/RPG/Program.cs
@@ -0,0 +1,54 @@
+﻿
+public class Unit
+{
+    private int maxHealth;
+    private int health;
+    private string name;
+    private int id;
+    private static int nextId;
+    
+    public Unit(string name, int maxHealth)
+    {
+        this.maxHealth = maxHealth;
+        this.name = name;
+        id = nextId;
+        health = maxHealth;
+        nextId++;
+        ReportStatus();
+    }
+   public void ReportStatus()
+    {
+        Console.WriteLine($"Unit #{id}: {name} - {health}/{maxHealth} Health");
+    }
+
+   public void SetHealth(int newHealth)
+   {
+       health = Math.Clamp(newHealth, 0 ,this.maxHealth);
+       ReportStatus();
+   }
+    
+   ~Unit()
+   {
+       Console.WriteLine($"Unit #{id}: {name} got finalized.");
+   }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Unit zombie = new Unit("Zombie", 100);
+        Unit vampire = new Unit("Vamprie",200);
+        Unit evilCat = new Unit("Evil Cat",50);
+        Unit leet = new Unit("Leet", 1337);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("What do you want Leet's Health to be?");
+            int newHP = Convert.ToInt32(Console.ReadLine());
+            leet.SetHealth(newHP);
+        }
+
+
+    }
+}

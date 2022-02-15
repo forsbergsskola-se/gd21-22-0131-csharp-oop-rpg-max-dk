de51eb4fc73eded7461e6210cf379f557ca08bf1
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-02-15 15:44:50 +0100

    Continued working on P2Classes

--- projects/P2Classes/Program.cs
+++ projects/P2Classes/Program.cs
@@ -1,22 +1,47 @@
-﻿
-    class Program
+﻿class Program
     {
         static void Main()
         {
             Person human = new Person();
             Animal dog = new Animal();
             Car volvo = new Car();
-
+            
             Console.WriteLine(human);
             Console.WriteLine(dog);
             Console.WriteLine(volvo);
+            
+
+            Person[] persons = new Person[3];
+            
+            
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i] = new Person();
+                Console.WriteLine("Give me a name.");
+                persons[i].name = Console.ReadLine();
+                
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i].IntroduceYourself();
+            }
+
         }
     }
 
-    class Person
+
+public class Person
+{
+    public string name;
+
+    public void IntroduceYourself()
     {
-        
+        Console.WriteLine($"Hello, my name is {name}");
     }
+}   
+
+   
 
     class Animal
     {
